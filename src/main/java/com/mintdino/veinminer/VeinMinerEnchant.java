0fbf1fc80223c819086d33d216e83d9b89ec6859
mintdinoyt <mintdinoyt@gmail.com>
2025-09-28 14:48:20 +0530

    Create VeinMinerEnchant.java

---
+++ src/main/java/com/mintdino/veinminer/VeinMinerEnchant.java
@@ -0,0 +1,20 @@
+package com.mintdino.veinminer;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class VeinMinerEnchant extends Enchantment {
+    public VeinMinerEnchant(NamespacedKey key) { super(key); }
+    @Override public String getName() { return "Vein Miner"; }
+    @Override public int getMaxLevel() { return 1; }
+    @Override public int getStartLevel() { return 1; }
+    @Override public EnchantmentTarget getItemTarget() { return EnchantmentTarget.TOOL; }
+    @Override public boolean isTreasure() { return false; }
+    @Override public boolean isCursed() { return false; }
+    @Override public boolean conflictsWith(Enchantment other) { return false; }
+    @Override public boolean canEnchantItem(ItemStack item) {
+        return item.getType().name().endsWith("_PICKAXE");
+    }
+}
